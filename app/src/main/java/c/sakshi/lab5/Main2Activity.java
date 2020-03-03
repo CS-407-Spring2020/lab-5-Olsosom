f6e8ca7f823918d13b919156a865f8ab8ed1f4f4
Sommer Olson <olsozom@gmail.com>
2020-03-03 14:44:27 -0600

    Commit for milestone 2

--- app/src/main/java/c/sakshi/lab5/Main2Activity.java
+++ app/src/main/java/c/sakshi/lab5/Main2Activity.java
@@ -6,30 +6,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
 
-    TextView name;
-
-    public void goToMain() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+    public static ArrayList<Note> notes = new ArrayList<>();
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        String str;
 
-        name = (TextView) findViewById(R.id.welcomeText);
-        Intent intent = getIntent();
-        str = intent.getStringExtra("message");
-        name.setText("Welcome " + str + "!");
+        // 1. Display welcome message
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("username", "");
+        textView = (TextView) findViewById(R.id.welcomeText);
+        textView.setText("Welcome " + name);
+
+        // 2. Get SQL Database
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        dbHelper.createTable();
+
+        // 3. Initiate the notes class
+        notes = dbHelper.readNotes(name);
+
+        // 4. Create an ArrayList<String> Iterator
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes) {
+            displayNotes.add(String.format("Title:%s\nDate:%s", note.getTitle(), note.getDate()));
+        }
+
+        // 5. Display Notes
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.notesListView);
+        listView.setAdapter(adapter);
+
+        // 6. Add item onClickListener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+                intent.putExtra("noteid", position);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -42,12 +75,18 @@ public class Main2Activity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.logout:
+                Intent intent = new Intent(this, MainActivity.class);
+                // Remove username reference
                 SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
                 sharedPreferences.edit().remove("username").apply();
-                goToMain();
+                // Go back to login screen
+                startActivity(intent);
                 return true;
 
             case R.id.addNote:
+                Intent intent2 = new Intent(this, Main3Activity.class);
+                intent2.putExtra("noteid", -1);
+                startActivity(intent2);
                 return true;
 
             default:
